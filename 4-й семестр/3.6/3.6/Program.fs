386eb0d40cc20e3ab591f35283b9457580966bd8
AlexanderLutsenko <Alexander-2807@mail.ru>
2013-03-08 02:31:21 +0400

    _

---
+++ 4-й семестр/3.6/3.6/Program.fs
@@ -0,0 +1,17 @@
+﻿let isPrime n =
+    let rec check i =
+        i > int(Operators.sqrt(float(n))) || (n % i <> 0 && check (i + 1))
+    check 2
+
+let primeNumerals = Seq.initInfinite (fun index ->
+    let rec findPrime index current acc = 
+        if acc = index then current-1
+        else
+            if isPrime current then 
+                let acc = acc+1
+                findPrime index (current+1) acc
+            else findPrime index (current+1) acc
+    findPrime (index+1) 2 0
+    )
+
+Seq.take 30 primeNumerals |> Seq.iter (printf "%A ")
