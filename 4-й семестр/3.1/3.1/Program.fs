c600210c1cd3db01aae9e9e5cebff64dee697b39
AlexanderLutsenko <Alexander-2807@mail.ru>
2013-03-07 22:59:13 +0400

    _

---
+++ 4-й семестр/3.1/3.1/Program.fs
@@ -0,0 +1,15 @@
+﻿let listSum list = 
+    let rec sum list a b currentIndex maxIndex maxValue = 
+        match list with
+        | hd::tl ->
+            if (a+hd) > maxValue then
+                let maxIndex = currentIndex-1
+                let maxValue = a+hd
+                sum tl b hd (currentIndex+1) maxIndex maxValue
+            else sum tl b hd (currentIndex+1) maxIndex maxValue
+        | [] -> maxIndex
+    match list with 
+    | h1::h2::tl -> sum tl h1 h2 3 0 0
+    | _ -> 0
+
+printfn "%A" (listSum [1; 5; 1; 2; 8; 13; 5; 10])
---
+++ 4-й семестр/3.2/3.2/Program.fs
@@ -0,0 +1,12 @@
+﻿let checkList list = 
+    let sortedList = List.sort list
+    let rec check list = 
+        match list with
+        | h1::h2::tl -> 
+            if h1 = h2 then false
+            else check (h2::tl)
+        | _ -> true
+    check sortedList
+        
+
+printfn "%A" (checkList [1; 5; -1; 2; 8; 13; 10])
