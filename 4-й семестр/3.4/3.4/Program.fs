8487bc42f6d969d95f5dc65b18ea6c6db3ac48aa
AlexanderLutsenko <Alexander-2807@mail.ru>
2013-03-08 14:38:26 +0400

    _

---
+++ 4-й семестр/3.4/3.4/Program.fs
@@ -0,0 +1,22 @@
+﻿type Tree =
+    | Tip
+    | Node of int * Tree * Tree
+
+let heightTree tree = 
+    let rec hTree tree acc =
+        match tree with
+        | Tip -> acc
+        | Node(value, left, right) ->
+            let hLeft = hTree left (acc+1)
+            let hRight = hTree right (acc+1)
+            if  hLeft > hRight then
+                hLeft
+            else hRight               
+    hTree tree 0
+
+let tree1 = Node(0, Node(1, Node(2, Tip, Tip), Node(3, Tip, Tip)), Node(4, Tip, Tip))
+let tree2 = Node(0, Tip, Node(4, Tip, Tip))
+let tree3 = Node(0, Tip, Tip)
+let tree4 = Tip
+
+printfn "%A" (heightTree tree1)
---
+++ 4-й семестр/3.5/3.5/Program.fs
@@ -0,0 +1,24 @@
+﻿exception OperatorError of string
+
+type Tree =
+    | Number of float
+    | Operation of char * Tree * Tree
+
+let solveTree tree = 
+    let rec sTree tree =
+        match tree with
+        | Number(value) -> value
+        | Operation(operator, left, right) ->
+            match operator with
+            | '+' -> sTree(left) + sTree(right)  
+            | '-' -> sTree(left) - sTree(right)  
+            | '*' -> sTree(left) * sTree(right)  
+            | '/' -> sTree(left) / sTree(right)   
+            | _ -> raise (OperatorError("unknown operator"))
+    sTree tree
+
+let tree1 = Operation('+', Operation('/', Number(3.), Number(6.)), Number(1.))
+let tree2 = Operation('*', Number(5.), Number(7.))
+let tree4 = Operation('+', Operation('?', Number(3.), Number(6.)), Number(1.))
+
+printfn "%A" (solveTree tree1)
