ac9cb12900e24fb13f823900c67c532998ea6b18
AlexanderLutsenko <Alexander-2807@mail.ru>
2013-03-16 22:54:36 +0400

    _

--- 4-й семестр/3.6/3.6/Program.fs
+++ 4-й семестр/3.6/3.6/Program.fs
@@ -3,20 +3,10 @@
         i > int (Operators.sqrt(float(n))) || (n % i <> 0 && check (i + 1))
     check 2
 
-let primeNumbers1 = Seq.initInfinite (fun index ->
-    let rec findPrime index current acc = 
-        if acc = index then current - 1
-        else
-            if isPrime current then findPrime index (current + 1) (acc + 1)
-            else findPrime index (current + 1) acc
-    findPrime (index + 1) 2 0
-    )
+let rec findPrime n =
+    if isPrime n then Some (n, n+1)
+    else findPrime (n+1)
 
-let primeNumbers2 = Seq.unfold (fun n -> let rec findPrime n =
-                                            if isPrime n then Some (n, n+1)
-                                            else findPrime (n+1)
-                                         findPrime n 
-                                         ) 2
+let primeNumbers = Seq.unfold (fun n -> findPrime n) 2
 
-Seq.take 100000 primeNumbers2 |> Seq.iter (printf "%A ")
-            
+Seq.take 100500 primeNumbers |> Seq.iter (printf "%A ")
